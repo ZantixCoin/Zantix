c8a92c2804e4eea3d1c97f41535d8252efc42fd2
Walter <s.heinhuis@outlook.com>
2018-09-14 10:44:04 +0200

    depends: Only use D_DARWIN_C_SOURCE when building miniupnpc on darwin

--- depends/packages/miniupnpc.mk
+++ depends/packages/miniupnpc.mk
@@ -6,7 +6,7 @@ $(package)_sha256_hash=d3c368627f5cdfb66d3ebd64ca39ba54d6ff14a61966dbecb8dd296b7
 
 define $(package)_set_vars
 $(package)_build_opts=CC="$($(package)_cc)"
-$(package)_build_opts_darwin=OS=Darwin LIBTOOL="$($(package)_libtool)"
+$(package)_build_opts_darwin=LIBTOOL="$($(package)_libtool)"
 $(package)_build_opts_mingw32=-f Makefile.mingw
 $(package)_build_env+=CFLAGS="$($(package)_cflags) $($(package)_cppflags)" AR="$($(package)_ar)"
 endef
